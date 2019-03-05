e74a75692009a53ed86ead20acd5d00dfd58d2d2
Mohamed Abdallah <Mohamedmabdallah14@gmail.com>
2019-03-05 10:21:48 -0800

    Implemented concentration game

--- Concentration/Controller/ViewController.swift
+++ Concentration/Controller/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var game = Concentration(numberOfPairsOfCards: (cardButtons.count + 1 ) / 2)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
+    
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
     @IBOutlet var cardButtons: [UIButton]!
     
     @IBAction func touchCard(_ sender: UIButton) {
-        
+       flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            game.chooseCard(at: cardNumber)
+            updateViewFromModel()
+        }
     }
     
+    func updateViewFromModel() {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUP {
+                button.setTitle(emoji(for: card) , for: .normal)
+                button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            } else {
+                button.setTitle("", for: .normal)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
+        }
+    }
+    
+    var emojiChoices = ["🎃","👻","🦇","😈", "😱", "🙀", "☠️", "💀"]
+    var emoji = [Int:String]()
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil, emojiChoices.count > 0 {
+           emoji[card.identifier] = emojiChoices.remove(at: Int.random(in: 0..<emojiChoices.count))
+        }
+        return emoji[card.identifier] ?? "?"
+    }
 }
 
--- Concentration/Module/Concentration.swift
+++ Concentration/Module/Concentration.swift
@@ -10,5 +10,33 @@ import Foundation
 
 class Concentration
 {
+    var cards = [Card]()
     
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index:Int){
+        if !cards[index].isMatched {
+            cards[index].isFaceUP = true
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                if cards[matchIndex].identifier == cards[index].identifier {
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUP = false
+                }
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCards: Int){
+        for _ in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+        //cards = cards.shuffled()
+    }
 }
